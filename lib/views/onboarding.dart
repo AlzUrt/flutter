f2be47722977214c96956950ba9c6921d2f66425
Amaury Chabert <chabert.amaury2002@gmail.com>
2023-04-03 16:11:42 +0200

    first page

--- lib/views/onboarding.dart
+++ lib/views/onboarding.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:padsou/assets/colors.dart';
+import 'package:padsou/ui/typo.dart';
+import 'package:padsou/components/buttonCard.dart';
+import 'package:padsou/components/slider/slider.dart';
+import 'package:padsou/components/ImageGrid.dart';
 
 class onBoarding extends StatefulWidget {
   @override
@@ -9,20 +14,58 @@ class AccueilState extends State<onBoarding> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Padsou"),
-      ),
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.all(8),
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("nom")],
+      backgroundColor: CustomColors.lightBlue,
+      body: Column(
+        children: [
+          const SizedBox(height: 80),
+          
+          Center(
+              child: Text(
+                'PAS DE SOUS ?',
+                style: CustomTextStyle.title(),
+              ),
+            ),
+          Center(
+              child: Text(
+                'Y’A PADSOU.',
+                style: CustomTextStyle.title(color: Colors.pink),
+              ),
+            ),
+            const Spacer(),
+          Column(
+              children: [
+                // Slider
+                MyCarousel(),
+
+                // Texte des bons plans
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 95),
+                  child: Text(
+                    'Accède aux 500 bons plans qu’on te propose chaque mois',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+          // Boutton C'est parti
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: SizedBox(
+                width: 261,
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: CustomButtonStyle.title(),
+                  child: const Text('C’est parti !'),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
