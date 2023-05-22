a2d8234d57a47c15c36b9ca4e151a1730c528101
MAZEL Florian <mazel.florian1@gmail.com>
2023-05-22 16:22:31 +0200

    Faut corriger les erreurs amaury

--- lib/firebase/firebase_listener.dart
+++ lib/firebase/firebase_listener.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
 
 // Méthode pour s'inscrire avec une adresse e-mail et un mot de passe
 Future<UserCredential?> signUp(String email, String password) async {
@@ -35,18 +39,44 @@ Future<UserCredential?> signIn(String email, String password) async {
     // Enregistrer l'événement d'analyse pour la connexion réussie
     await FirebaseAnalytics.instance.logEvent(name: 'sign_in');
 
+    // Envoyer une notification FCM à vous-même pour indiquer que l'utilisateur s'est connecté avec succès
+    String serverKey =
+        'AAAAiuI83Y8:APA91bFxGj9J1qvS9GqVR-wlzgAhWxusbFoX4AZ8SNdXSfuXHq70yG0P7BXMmnL8HEUAj2YhHBH7bBqAm6JW9XQDQGaPI4uVbbWIRI_f6nm5YtNuBaRK0O3Eq4GLfBtVLegWEvndjPUW';
+    String? fcmToken = await FirebaseMessaging.instance.getToken();
+
+    final response = await http.post(
+      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=$serverKey',
+      },
+      body: jsonEncode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{
+            'body': 'Vous êtes connecté',
+            'title': 'Padsou'
+          },
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done'
+          },
+          'to': fcmToken,
+        },
+      ),
+    );
+    if (response.statusCode == 200) {
+    } else {}
+
     return userCredential;
   } on FirebaseAuthException catch (e) {
     if (e.code == 'user-not-found') {
-      print('Aucun utilisateur correspondant à cet e-mail.');
-    } else if (e.code == 'wrong-password') {
-      print('Mot de passe incorrect.');
-    }
+    } else if (e.code == 'wrong-password') {}
     // Gérer d'autres erreurs Firebase ici
 
     return null;
   } catch (e) {
-    print(e.toString());
     return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:padsou/firebase/firebase_options.dart';
 import 'package:padsou/views/login.dart';
@@ -11,7 +12,31 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  String? token = await FirebaseMessaging.instance.getToken();
+  print('User FCM Token: $token');
 
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    if (notification != null && android != null) {
+      print('Message title: ${notification.title}, body: ${notification.body}');
+    }
+  });
   runApp(MyApp(analytics: analytics));
 }
 
